997c3b18ab660d933c9825ac4f4e5c8efb9ce1db
northberry <145092398+northberry@users.noreply.github.com>
2023-09-15 15:31:23 +1000

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinish & Roket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
