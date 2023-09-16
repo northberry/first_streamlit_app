2f8f7add5296d3831779f710a4267823dc9c64ad
northberry <145092398+northberry@users.noreply.github.com>
2023-09-16 15:42:38 +1000

    Update streamlit_app.py
    -rerun

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -37,7 +36,7 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 # to stop streamlit for troubleshooting
 streamlit.stop()
