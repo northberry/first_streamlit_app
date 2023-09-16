69d77646a90aa59b888826a7406a90eaa60dd30b
northberry <145092398+northberry@users.noreply.github.com>
2023-09-16 15:05:39 +1000

    Update streamlit_app.py
    -change again

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,9 @@ my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") # test to check account
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:") 
+streamlit.dataframe(my_data_row)
 # streamlit.text("Hello from Snowflake:") # test greeting
-streamlit.text("The fruit load list contains:") 
-streamlit.text(my_data_row)
+# streamlit.text("The fruit load list contains:") 
+# streamlit.text(my_data_row)
+
